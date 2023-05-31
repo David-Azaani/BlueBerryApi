7951a57998edc8d4e4558b4e8d85a14c20bdf825
f00000b <f00000b@gmail.com>
2023-05-31 20:26:52 +0330

    Adding Name to identitytable,and changing in program cs and dbconterxt,fixinf stringConnection

--- BlueBerry_API/BlueBerry_API/Data/ApplicationDbContext.cs
+++ BlueBerry_API/BlueBerry_API/Data/ApplicationDbContext.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using BlueBerry_API.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BlueBerry_API.Data
 {
-    public class ApplicationDbContext:IdentityDbContext
+    public class ApplicationDbContext:IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions options):base(options)
         {
             
         }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
---
+++ BlueBerry_API/BlueBerry_API/Model/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BlueBerry_API.Model
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string Name { get; set; }
+    }
+}
