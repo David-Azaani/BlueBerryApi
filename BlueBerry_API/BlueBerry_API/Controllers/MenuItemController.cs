55614c737880c84dc22e5be6c1f502038e076446
f00000b <f00000b@gmail.com>
2023-05-31 22:05:18 +0330

    adding ApiResponse,and Getting Uinq Answers from MenuItem Controller

---
+++ BlueBerry_API/BlueBerry_API/Controllers/MenuItemController.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using BlueBerry_API.Data;
+using BlueBerry_API.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlueBerry_API.Controllers
+{
+    [Route("api/MenuItem")]
+    [ApiController]
+    public class MenuItemController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly ApiResponse _response;
+
+        public MenuItemController(ApplicationDbContext db)
+        {
+            _db = db;
+            _response = new ApiResponse();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMenuItems()
+        {
+            try
+            {
+                _response.Result = await _db.MenuItems.ToListAsync();
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
+            }
+            catch (Exception e)
+            {
+                _response.Result = e.Message;
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                return BadRequest(_response);
+
+            }
+
+        }
+
+
+
+    }
+}
