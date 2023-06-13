a13d33979d54fc0047083c92d666058de32bfe34
f00000b <f00000b@gmail.com>
2023-06-13 18:14:30 +0330

    prepareing for Authentication anAuthorization Processs : Aadding sercert,registerand login Dto

---
+++ BlueBerry_API/BlueBerry_API/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using BlueBerry_API.Data;
+using BlueBerry_API.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace BlueBerry_API.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        private string _secretKey;
+
+        private ApiResponse _response;
+        public AuthController(ApplicationDbContext db, IConfiguration configuration)
+        {
+            _db = db;
+            _secretKey = configuration.GetValue<string>("ApiSettings:Secret");
+            _response = new ApiResponse();
+        }
+    }
+}
