534d6d19a85de5025b90250daa9d7721b303175d
f00000b <f00000b@gmail.com>
2023-06-13 17:32:03 +0330

    adding login register dto

---
+++ BlueBerry_API/BlueBerry_API/Model/Dto/RegisterRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace BlueBerry_API.Model.Dto
+{
+    public class RegisterRequestDTO
+    {
+        public string  Username { get; set; }
+        public string  Name { get; set; }
+        public string  Password { get; set; }
+        public string  Role { get; set; }
+    }
+}
